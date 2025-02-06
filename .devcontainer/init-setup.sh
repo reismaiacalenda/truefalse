bf6b45832eb18a6a5cecde1f18c4ddf81881a8c4
maiareismaia <lucas@reismaia.com>
2025-02-06 01:24:49 +0000

    adiciona script de inicialização para configuração do ambiente com nvm e Angular CLI

--- .devcontainer/init-setup.sh
+++ .devcontainer/init-setup.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
 nvm install 22.13.1
 nvm use 22.13.1
 nvm alias default 22.13.1
 npm install -g npm@11.1.0
-npm install -g @angular/cli@19.1.6"
+npm install -g @angular/cli@19.1.6
+
+#apt-get update && sudo apt-get upgrade -y
+#apt-get install -y gcc-11 g++-11 libstdc++6
+#apt-get install --reinstall libstdc++6
